635d7bb22b645bc6c9973eb8db8d9e726e252541
heshibin <17608484341@163.com>
2023-01-01 23:44:39 +0800

    c

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/JAVA">
+            to="/docs/intro">
             Word Loading
           </Link>
         </div>
@@ -33,7 +33,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        {/* <HomepageFeatures /> */}
       </main>
     </Layout>
   );
