fa804b0e1bfdfd3b1252388b9779b8589fc75331
heshibin <17608484341@163.com>
2023-01-05 19:45:54 +0800

    c

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,32 +4,30 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: '',
     // Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: '',
     // Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        成功需要成本，时间也是一种成本，
+        对时间的珍惜就是对成本的节约。
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: '',
     // Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        
       </>
     ),
   },
