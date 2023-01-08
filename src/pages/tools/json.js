cf3ed21675a63069dfe95415cf97a77b26a8f759
heshibin <17608484341@163.com>
2023-01-08 16:59:51 +0800

    c

--- src/pages/tools/json.js
+++ src/pages/tools/json.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import Layout from '@theme/Layout';
-import { Col, Input, Row, Alert, Typography  } from 'antd';
+import { Col, Input, Row, Alert, Typography, Button  } from 'antd';
 const { TextArea } = Input;
 const { Title, Paragraph, Text, Link } = Typography;
 export default class ReactJsonView extends React.Component{
@@ -10,30 +10,39 @@ export default class ReactJsonView extends React.Component{
     constructor(props){
         super(props)
         this.state = {
+            text: "",
             mockJson:{},
             errorMsg:''
         }
 
         this.handleChange = this.handleChange.bind(this)
+        this.compressJsonStr = this.compressJsonStr.bind(this)
     }
     
     //改变textarea内容
     handleChange(e){    
         const str = e.target.value;
         this.setState({
-            mockJson: str,
+            text: str,
+            mockJson: str
+        })
+    }
+
+    compressJsonStr() {
+        var str = JSON.stringify(JSON.parse(this.state.text));
+        this.setState({
+            text: str
         })
     }
 
     render() {
-        let { mockJson, errorMsg } = this.state;
+        let { mockJson, errorMsg, text } = this.state;
         try {
             errorMsg = '';
             if(mockJson.length > 0) {
                 mockJson =  JSON.parse(mockJson);
             }
         } catch(e) {
-            console.log(e.message);
             mockJson = mockJson;
             errorMsg = e.message;
         }
@@ -43,24 +52,27 @@ export default class ReactJsonView extends React.Component{
                 title={`JSON在线格式化工具`}
                 description="JSON在线格式化工具">
 
-                <Typography style={{margin: '10px'}}>
+                <div style={{margin: '10px'}}>
+                <Typography>
                     <Title>JSON在线格式化</Title>
                     <Paragraph>
                         采用<Text code>react-json-view</Text>实现。<Link href="https://github.com/heshibin/heshibin.github.io/blob/main/src/pages/tools/json.js">查看源码</Link>
                     </Paragraph>
                 </Typography>
                 {errorMsg.length > 0 && (
-                     <Alert message={errorMsg} type="error" showIcon banner />
+                    <Alert message={errorMsg} type="error" showIcon banner />
                 )}
-                <Row style={{margin: '10px'}}>
+
+                <Button type="primary" onClick={this.compressJsonStr}>压缩</Button>
+
+                <Row style={{marginTop: '10px'}}>
                     <Col flex={2}>
-                        <TextArea rows={20} placeholder="请输入JSON数据..." onChange={this.handleChange}/>
+                        <TextArea rows={20} placeholder="请输入JSON数据..." onChange={this.handleChange} value={text}/>
                     </Col>
                     <Col flex={3}>
                     <BrowserOnly>
                         {() => {
                             const ReactJson = require('react-json-view').default;
-                            console.log(ReactJson);
                             return <ReactJson
                                 collapsed={false}//是否收起,true为收起
                                 indentWidth={10}//缩进
@@ -84,81 +96,8 @@ export default class ReactJsonView extends React.Component{
                         </BrowserOnly>
                     </Col>
                 </Row>
+                </div>
         </Layout>
         )
     }
 }
-// export default class ReactJsonView extends React.Component{
-//     constructor(props){
-//         super(props)
-//         this.state = {
-//             mockJson:{},
-//             errorMsg:''
-//         }
-
-//         this.handleChange = this.handleChange.bind(this)
-//     }
-    
-//     //改变textarea内容
-//     handleChange(e){    
-//         const str = e.target.value;
-//         this.setState({
-//             mockJson: str,
-//         })
-//     }
-
-//     render(){
-//         let { mockJson, errorMsg } = this.state;
-//         try {
-//             errorMsg = '';
-//             if(mockJson.length > 0) {
-//                 mockJson =  JSON.parse(mockJson);
-//             }
-//         } catch(e) {
-//             console.log(e.message);
-//             mockJson = mockJson;
-//             errorMsg = e.message;
-//         }
-//         return(
-//             <Layout
-//                 title={`JSON在线格式化工具`}
-//                 description="JSON在线格式化工具">
-//                 <Typography style={{margin: '10px'}}>
-//                     <Title>JSON在线格式化</Title>
-//                     <Paragraph>
-//                         采用<Text code>react-json-view</Text>实现。
-//                     </Paragraph>
-//                 </Typography>
-//                 {errorMsg.length > 0 && (
-//                     <Alert message={errorMsg} type="error" showIcon banner />
-//                 )}
-//                 <Row style={{margin: '10px'}}>
-//                     <Col flex={2}>
-//                         <TextArea rows={20} placeholder="请输入JSON数据..." onChange={this.handleChange}/>
-//                     </Col>
-//                     <Col flex={3}>
-//                         <ReactJson
-//                             collapsed={false}//是否收起,true为收起
-//                             indentWidth={10}//缩进
-//                             iconStyle='circle'
-//                             src={mockJson}
-//                             theme='monokai'
-//                             collapseStringsAfterLength={10}//字符串多长时用省略号
-//                             enableClipboard={true}//点击向左箭头进行复制
-//                             displayObjectSize={false}//显示有多少个items属性
-//                             displayDataTypes={false}//显示值的类型
-//                             onEdit={(edit)=>{console.log(edit)}}//编辑完成前调用回调函数
-//                             onAdd={(add)=>{console.log(add)}}//添加属性
-//                             defaultValue='malinshu'//添加属性后的默认值
-//                             onDelete={(onDelete)=>{console.log(onDelete)}}//删除属性
-//                             onSelect={(onSelect)=>{console.log(onSelect)}}//单击键值对的值时触发
-//                             sortKeys={true}//键的排序
-//                             quotesOnKeys={false}//是否显示键的引号
-//                             groupArraysAfterLength={5}//数组为多少个的时候被拆分显示
-//                             />
-//                     </Col>
-//                 </Row>
-//             </Layout>
-//         )
-//     }
-// }
