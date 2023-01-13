78114f32b363a1bfddafea87acb1b0df21e9fe9b
heshibin <49633468+heshibin@users.noreply.github.com>
2023-01-13 14:24:17 +0800

    Update DocCardList.js

--- src/components/DocCardList.js
+++ src/components/DocCardList.js
@@ -7,46 +7,36 @@ export default function DocCardList() {
     <div className="site-card-wrapper">
       <Row gutter={16}>
         <Col span={8}>
-          <Card title={()=> (<>
-                JAVA
-                <Link> href="/JAVA/知识点/2023-01/01什么是try-with-resource">
-                  前往
-                </Link>
-              </>)} bordered={false}>
+          <Card title="JAVA" bordered={false}>
             常用java工具类，知识点等
+                 <Link> href="/JAVA/知识点/2023-01/01什么是try-with-resource">
+                  前往
+                </Link>
           </Card>
         </Col>
         <Col span={8}>
-          <Card title={()=>(<>
-                前端
-                <Link> href="/前端/react相关">
-                  前往
-                </Link>
-              </>)} bordered={false}>
+          <Card title="前端" bordered={false}>
             javascript、jquery、html、react等
+             <Link> href="/前端/react相关">
+                  前往
+                </Link>
           </Card>
         </Col>
         <Col span={8}>
-          <Card title={()=>(<>
-                工具
-                <Link> href="/download">
+          <Card title="工具" bordered={false}>
+            一些开发工具等<Link> href="/download">
                   前往
                 </Link>
-              </>)} bordered={false}>
-            一些开发工具等
           </Card>
         </Col>
       </Row>
 
       <Row gutter={16}>
         <Col span={8}>
-          <Card title={()=>(<>
-                面试题
-                <Link> href="/面试题/zookeeper">
+          <Card title="面试题" bordered={false}>
+            java基础面试题...<Link> href="/面试题/zookeeper">
                   前往
                 </Link>
-              </>)} bordered={false}>
-            java基础面试题...
           </Card>
         </Col>
         <Col span={8}>
