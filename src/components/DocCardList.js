ffbe52fcc8ba0e7116a203aaa155e4e4fb94b75c
heshibin <17608484341@163.com>
2023-01-31 16:42:20 +0800

    c

--- src/components/DocCardList.js
+++ src/components/DocCardList.js
@@ -9,7 +9,7 @@ export default function DocCardList() {
 
       <Card style={{ textAlign: 'center', boxShadow: 'none' }} bordered={false}>
         <Avatar
-          size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+          size={64}
           src={"/img/avatar.jpeg"}
         />
 
