648682e5eed3c91644f8d6c3b4c96c1e58f7a707
Machu <49633468+heshibin@users.noreply.github.com>
2023-02-25 19:33:05 +0800

    Update index.js

--- src/theme/Footer/Layout/index.js
+++ src/theme/Footer/Layout/index.js
@@ -48,7 +48,7 @@ export default function FooterLayout({style, links, logo, copyright}) {
           <div className="footer__bottom text--center">
             {logo && <div className="margin-bottom--sm">{logo}</div>}
             {copyright}
-            <a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备2022003734号</a>
+            <a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备2022003734号-1</a>
             {runTimeTxt}
           </div>
         )}
