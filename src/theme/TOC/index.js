0cff6369673b2c749d3eecca170a420e0b94c708
heshibin <17608484341@163.com>
2023-02-26 13:49:39 +0800

    c

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import TOCItems from '@theme/TOCItems';
 import styles from './styles.module.css';
-import {Adsense} from '@ctrl/react-adsense';
+// import {Adsense} from '@ctrl/react-adsense';
 // Using a custom className
 // This prevents TOCInline/TOCCollapsible getting highlighted by mistake
 const LINK_CLASS_NAME = 'table-of-contents__link toc-highlight';
@@ -15,10 +15,10 @@ export default function TOC({className, ...props}) {
           linkClassName={LINK_CLASS_NAME}
           linkActiveClassName={LINK_ACTIVE_CLASS_NAME}
         />
-        <Adsense
+        {/* <Adsense
         client="ca-pub-6880859209937012"
         slot="6835935691"
-        />
+        /> */}
     </div>
   );
 }
